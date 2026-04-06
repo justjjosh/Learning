f9c5ea880f8d79518964fe186039475c3f8bbb48
Raji Olatubosun Joshua <rajiolatubosun@gmail.com>
2026-04-06 01:32:17 +0100

    solved isAnagram leetcode problem

--- leet.py
+++ leet.py
@@ -8,10 +8,10 @@ class Solution(object):
                 return [found_numbers[compliment], index]
             found_numbers[current_number] = index
 
-test = Solution()
+"""test = Solution()
 nums = [3,2,4]
 target = 6
-print(test.twoSum(nums, target))
+print(test.twoSum(nums, target))"""
 
 #contains duplicate
 class Solutions(object):
@@ -23,6 +23,37 @@ class Solutions(object):
             found_numbers.add(value)
         return False
     
-test = Solutions()
+"""test = Solutions()
 nums = [3,2,4,3]
-print(test.containsDuplicate(nums))
+print(test.containsDuplicate(nums))"""
+
+
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        if len(s) != len(t):
+            return False
+        
+        counts = {}
+        for letter in s:
+            if letter in counts:
+                counts[letter] += 1
+            else:
+                counts[letter] = 1
+
+        search = {}
+        for letter in t:
+            if letter in search:
+                search[letter] += 1
+            else:
+                search[letter] = 1
+
+        if counts == search:
+            return True
+        else:
+            return False
+
+
+s = "car"
+t = "crr"
+test = Solution()
+print(test.isAnagram(s, t))
